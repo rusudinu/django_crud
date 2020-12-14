a088fc5e9c817c44d5d2105c7e4cf478367e31f7
Rusu Dinu Stefan <dinu@codingshadows.com>
2020-12-14 10:59:14 +0200

    created a base.html file, to not write twice the header for regular website files

--- django_crud/settings.py
+++ django_crud/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = '@&c9iq8e32e=j(9^$g0myn!yw=oc442yirs8b_w8)8&h-l_kj7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = True  # set this to False for the custom error pages to work
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []  # '127.0.0.1'
 
 # Application definition
 
--- django_crud/urls.py
+++ django_crud/urls.py
@@ -1,25 +1,13 @@
-"""django_crud URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
 from .crud_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('404_test/', views.error_404),
+    path('test_404/', views.error_404),
+    path('test_403/', views.error_403),
+    path('test_400/', views.error_400),
+    path('test_500/', views.error_500),
 ]
 
 handler404 = views.error_404
